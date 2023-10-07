866c78c3b2018c96ca62855d8d608319b484b633
Janek <eunho.moon@gmail.com>
2023-10-07 14:55:44 +0900

    init

---
+++ type/01.string.py
@@ -0,0 +1,18 @@
+# replace
+a = '오늘 냘씨는 흐림 입니다.'.replace('흐림', '맑음')
+print(a)
+
+# find
+b = 'Hello, World!'.find('World')
+print(b)  # 없을 경우 -1
+
+# split
+c = 'apple, pear, grape, pineapple'.split(', ')
+print(c)
+
+# strip
+d = '   Hello, World!   '.strip()
+print(d)
+
+e = '   Hello, World!'.lstrip()
+f = 'Hello, World!   '.rstrip()
