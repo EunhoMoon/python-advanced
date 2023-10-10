a10faf67b1ec492b04bfb30f407f7f86fa1f432e
Janek <eunho.moon@gmail.com>
2023-10-10 21:39:14 +0900

    list 내포

--- type/03.list.py
+++ type/03.list.py
@@ -31,5 +31,31 @@ print(numbers)
 
 # enumerate
 titles = ['아이언맨', '스파이더맨', '토르', '헐크']
-for index, title in enumerate(titles, 1): # 두번째 인자 = 인덱스 시작 번호
+for index, title in enumerate(titles, 1):  # 두번째 인자 = 인덱스 시작 번호
     print(index, title)
+
+# list 내포
+numbers = [i for i in range(5)]
+print(numbers)
+
+numbers2 = [100, 200, 300]
+double_numbers = [i * 2 for i in numbers2]
+print(double_numbers)
+
+# 조건을 활용한 list 내포
+numbers3 = [i for i in range(10) if i % 2 == 0]
+print(numbers3)
+
+numbers4 = [100, 200, 300, 400, 500]
+double_numbers2 = [i * 2 for i in numbers4 if i >= 300]
+print(double_numbers2)
+
+# 실습
+a = ["apple", "watch", "apollo", "star", "avocado"]
+b = [i for i in a if i.startswith('a')]
+print(b)
+
+# 실습2: else 사용시 for문은 if문 뒤에 위치
+c = ["오메가3", None, "비타민D", None, "비타민C"]
+d = [i if i is not None else '' for i in c]
+print(d)
